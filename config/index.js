ca165defa2e2c11ba2102001bb778462150ee2bf
William Obeng <obeng.kojo.william@gmail.com>
2017-11-26 15:24:08 +0000

    chore():clean up project

--- config/index.js
+++
@@ -1,8 +0,0 @@
-// Here you can load config here.
-// Dont forget to add validations if any for all config that will obtaine from process.env
-const config = {}
-
-module.exports = env => {
-  if (config[env] === undefined) throw new Error(`No config for env variable ${env}`)
-  return config[env]
-}
